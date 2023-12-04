06021b70269bdb4255ed79c1401e44acefa9f397
Isaac <isaactakamura107@gmail.com>
2023-12-04 14:17:19 -0500

    Creacion de la tabla modelo venta

--- src/main/java/com/nicolas/hostal/vista/ventas/VentasTableModel.java
+++ src/main/java/com/nicolas/hostal/vista/ventas/VentasTableModel.java
@@ -4,10 +4,56 @@
  */
 package com.nicolas.hostal.vista.ventas;
 
+import com.nicolas.hostal.modelo.Venta;
+import java.util.List;  
+import java.util.ArrayList;
+import com.nicolas.hostal.servicios.ServManager;
+import com.nicolas.hostal.servicios.VentaServicio;
+import javax.swing.table.AbstractTableModel;
+
 /**
  *
- * @author NICOLAS
+ * author NICOLAS
  */
-public class VentasTableModel {
+public class VentasTableModel extends AbstractTableModel {
+
+    private VentaServicio servicio;
+
+    private List<Venta> datos = new ArrayList<>();
+
+    public VentasTableModel() {
+        this.servicio = new VentaServicio()
+    }
+
+    public void updateModel() {
+        datos = servicio.obtenerTodasVentas();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        switch (column) {
+            case 0: return "ID";
+            default: return "[no]";
+        }
+    }
     
+    @Override
+    public int getRowCount() {
+        return datos.size();
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Venta preguntado = datos.get(rowIndex);
+        switch (columnIndex) {
+            case 0: return preguntado.getId();
+            default: return "";
+        }
+    }
 }
+
